17eab42b7e070918a13e4f5c8e90343a117e857f
wzz2010 <h20060222@qq.com>
2020-06-04 23:08:42 +0800

    2

--- pythoncode/task_class/Animal.py
+++ pythoncode/task_class/Animal.py
@@ -1,13 +1,21 @@
 class Animal():
 
-    def __init__(self,name,color):
+    def __init__(self,name,color,age,sex):
         self.name=name
         self.color=color
-        # self.age=age
-        # self.sex=sex
+        self.age=age
+        self.sex=sex
 
     def run(self):
-        print(f"{self.color}的{self.name}会跑")
+        print(f"{self.name} {self.color} {self.age} {self.sex} 会跑")
 
     def shout(self):
-        print(f"{self.color}的{self.name}会叫")
+        print(f"{self.name} {self.color} {self.age} {self.sex} 会叫")
+
+if __name__=="__main__":
+    animal_a=Animal(name="兔子",
+                    color="白色",
+                    age="3个月大",
+                    sex="公的")
+    animal_a.run()
+    animal_a.shout()
--- pythoncode/task_class/cat.py
+++ pythoncode/task_class/cat.py
@@ -1,10 +1,21 @@
 from Animal import Animal
 
 class cat(Animal):
-    def __init__(self,name,color,age,sex):
+    def __init__(self,color,age,sex):
+        name = "猫"
         Animal.__init__(self,name,color,age,sex)
         self.hair="短毛"
     def catch_rat(self):
-        print("捉老鼠")
+        print(f"{self.name} {self.color} {self.age} {self.sex} {self.hair} 会捉老鼠")
+
     def shout(self):
-        print("喵喵叫")
+        print(f"{self.name} {self.color} {self.age} {self.sex} {self.hair} 会喵喵叫")
+
+if __name__=="__main__":
+    cat_a=cat(color='黑色',
+              age='半年的',
+              sex='母的')
+    cat_a.catch_rat()
+    cat_a.shout()
+
+
